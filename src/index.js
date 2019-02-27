5a123408d006fad27ad5cece4e2638c3a14d379d
Highbrainer <alexandre.borgoltz@gmail.com>
2019-02-27 15:28:40 +0100

    first working version

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
-class PublicStorage {
+class PublicStorageAccess {
 
 	 constructor () {
 		 this.IFRAME_ROOT_URL = "https://publicstorage.neocities.org/shared-iframe.html";
+		 this.uid = this.uniqueId();
 	 }
-
-	 uniqueId(){
-	  function chr4(){
-	    return Math.random().toString(16).slice(-4);
-	  }
-	  return chr4() + chr4() +
-	    '-' + chr4() +
-	    '-' + chr4() +
-	    '-' + chr4() +
-	    '-' + chr4() + chr4() + chr4();
+	 
+	uniqueId() {
+		function chr4(){
+			return Math.random().toString(16).slice(-4);
+			}
+		return chr4() + chr4() +
+				'-' + chr4() +
+				'-' + chr4() +
+				'-' + chr4() +
+				'-' + chr4() + chr4() + chr4();
 	}
 
 	 prepareIFrame() {
 		var that = this;
+		var iframe = document.createElement("iframe");
+		iframe.id=that.uid;
+		iframe.src=that.IFRAME_ROOT_URL + "?uid=init-"+that.uid;
+		iframe.style="display:none;";
 		return new Promise(function(resolve, reject) {
 			console.debug("Je démarre...");
 			window.addEventListener('message', function mafunc(tkn) {
 				
-				console.debug("prepareIFrame " + that.iframe.id + " reçoit un message... " + tkn.data);
+				console.debug("prepareIFrame " + that.uid + " reçoit un message... " + tkn.data);
 				
 				if (that.IFRAME_ROOT_URL.indexOf(tkn.origin)<0) {
 					return;
@@ -30,14 +35,14 @@ class PublicStorage {
 				try {
 					var packet = JSON.parse(tkn.data);
 					
-					if(!(packet.frameId === that.iframe.id)) {
+					if(!(packet.frameId === "init-" + that.uid)) {
 						// ignore
 						return;
 					}
 					
-					console.debug("prepareIFrame " + that.iframe.id + " accepte un message... " + tkn.data);
+					console.debug("prepareIFrame " + that.uid + " accepte un message... " + tkn.data);
 					if(packet.ready) {
-						resolve(that.iframe);					
+						resolve(iframe);					
 					}
 				} catch (e) {
 					reject(tkn.data);
@@ -45,85 +50,83 @@ class PublicStorage {
 				window.removeEventListener('message', mafunc);
 				// that.body.removeChild(that.iframe);
 		    });
-			that.iframe = document.createElement("iframe");
-			that.iframe.id=that.uniqueId();
-			that.iframe.src=that.IFRAME_ROOT_URL + "?uid="+that.iframe.id;
-			that.iframe.style="display:none;";
-			that.body = document.getElementsByTagName("body")[0]; 
-			that.body.appendChild(that.iframe);
+			document.getElementsByTagName("body")[0].appendChild(iframe);
 
-			setTimeout(()=>reject("TIMEOUTED!"), 20000);
+			setTimeout(()=>reject(`Request ${that.uid} TIMEOUTED!`), 20000);
 		});
 	}
- access(access, prop, value = null, level = "local") {
+	 
+	 access(access, prop, value = null, level = "local") {
+			
+			if(!(access === "get" || access === "set" || access === "delete")) {
+				throw new Error("access can only be 'set', 'get' or 'delete' - not '" + access + "'");
+			}
+			
+			if (!prop) {
+				throw new Error("Prop name is mandatory");
+			}
+			
+			if(!(level === "local" || level === "session")) {
+				throw new Error("level can only be 'session' or 'local' - not '" + access + "'");
+			}
+				
+			var that = this;
+			
+			var promise = new Promise(function(resolve, reject) {
+				that.prepareIFrame().then(iframe => {
+					window.addEventListener('message', function mafunc(tkn) {
+						if (that.IFRAME_ROOT_URL.indexOf(tkn.origin)<0) {
+							return;
+						}
+						try {
+							var packet = JSON.parse(tkn.data);
+							
+							if(!(packet.uid === that.uid)) {
+								// ignore
+								return;
+							}
+//							
+		// if(!packet.body) {
+		// // ignore
+		// return;
+		// }
+							console.debug("Access " + that.uid + " reçoit un message... " + tkn.data);
+							resolve(packet.body);
+			
+							console.debug("Je résouds!");
+						} catch (e) {
+							reject(tkn.data);
+						}
+						//that.body = document.getElementsByTagName("body")[0];
+						iframe.parentNode.removeChild(iframe);
+						window.removeEventListener('message', mafunc);
+				    });
+			
+					console.debug("On envoit une request... frameId: init-" + that.uid + "\trequestId: " + that.uid);
+					var request = {uid:that.uid, access:access, prop:prop, value:value, level:level};
+					console.debug(request);
+					iframe.contentWindow.postMessage(JSON.stringify(request), '*');
+					setTimeout(()=>reject("TIMEOUTED!"), 20000);
+				});
+			});
+			return promise;
+		}
 	
-	if(!(access === "get" || access === "set" || access === "delete")) {
-		throw new Error("access can only be 'set', 'get' or 'delete' - not '" + access + "'");
-	}
-	
-	if (!prop) {
-		throw new Error("Prop name is mandatory");
-	}
-	
-	if(!(level === "local" || level === "session")) {
-		throw new Error("level can only be 'session' or 'local' - not '" + access + "'");
-	}
-		
-	var requestId = this.uniqueId(); 
-	var that = this;
-	
-	var promise = new Promise(function(resolve, reject) {
-		console.debug("Je démarre... " + that);
-		that.prepareIFrame().then(iframe => {
-			window.addEventListener('message', function mafunc(tkn) {
-				if (that.IFRAME_ROOT_URL.indexOf(tkn.origin)<0) {
-					return;
-				}
-				try {
-					var packet = JSON.parse(tkn.data);
-					
-					if(!(packet.uid === requestId)) {
-						// ignore
-						return;
-					}
-//					
-//					if(!packet.body) {
-//						// ignore
-//						return;
-//					}
-					console.debug("Access " + requestId + " reçoit un message... " + tkn.data);
-					resolve(packet.body);
-	
-					console.debug("Je résouds!");
-				} catch (e) {
-					reject(tkn.data);
-				}
-				that.body = document.getElementsByTagName("body")[0];
-				that.body.removeChild(that.iframe);
-				window.removeEventListener('message', mafunc);
-		    });
-	
-			console.debug("On envoit une request... frameId: " + that.iframe.id + "\trequestId: " + requestId);
-			var request = {uid:requestId, access:access, prop:prop, value:value, level:level};
-			console.debug(request);
-			that.iframe.contentWindow.postMessage(JSON.stringify(request), '*');
-			setTimeout(()=>reject("TIMEOUTED!"), 20000);
-		});
-	});
-	return promise;
 }
 
+class PublicStorage { 
+
  sessionGet(prop) {
-	 return this.access("get", prop, null, "session");
+	 return new PublicStorageAccess().access("get", prop, null, "session");
  }
  sessionSet(prop, value) {
-	 return this.access("set", prop, value, "session");
+	 return new PublicStorageAccess().access("set", prop, value, "session");
  }
  localGet(prop) {
-	 return this.access("get", prop, null, "local");
+	 return new PublicStorageAccess().access("get", prop, null, "local");
  }
  localSet(prop, value) {
-	 return this.access("set", prop, value, "local");
+	 return new PublicStorageAccess().access("set", prop, value, "local");
  }
  get(prop) {
 	 return this.localGet(prop);
@@ -137,6 +140,17 @@ class PublicStorage {
 
 const publicstorage = new PublicStorage();
 
-window.publicstorage = publicstorage;
+function onLoadThen() {
+	return new Promise(function(resolve, reject) {
+		window.addEventListener('load', function unregisterme() {
+			resolve(publicstorage);
+			if (window) {
+				window.publicstorage = publicstorage;
+			}
+			window.removeEventListener('load', unregisterme);
+		});
+		setTimeout(function() {reject(new Error("Timeout waiting for onLoad!"));}, 10000);
+	});
+}
 
-module.exports = publicstorage;
+module.exports = onLoadThen();
